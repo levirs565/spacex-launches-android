90cfa5b5bbafb418010e68b4e02722023527ed41
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-02 11:32:50 +0700

    add missing jar libs dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(Dependencies.kotlin)
     implementation(Dependencies.coreKtx)
     testImplementation(Dependencies.junit)
