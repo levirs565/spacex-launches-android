610cc38efbabf74fff7ffbb69b2641c143b2be66
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-03 15:31:22 +0700

    domain: add creator for resultstate

--- core/src/main/java/com/levirs/spacexlaunches/domain/util/ResultState.kt
+++ core/src/main/java/com/levirs/spacexlaunches/domain/util/ResultState.kt
@@ -1,6 +1,12 @@
 package com.levirs.spacexlaunches.domain.util
 
-data class ResultState<T>(val data: T?, val error: T?) {
+data class ResultState<T>(val data: T?, val error: String?) {
+    companion object {
+        fun <T> succes(data: T) = ResultState(data, null)
+        fun <T> loading() = ResultState<T>(null, null)
+        fun <T> error(error: String) = ResultState<T>(null, error)
+    }
+
     fun isLoading() = data == null && error == null
     fun isError() = data == null && error != null
     fun isSuccess() = data != null && error == null
