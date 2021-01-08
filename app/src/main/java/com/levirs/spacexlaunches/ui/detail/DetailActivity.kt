12c6967c6dfa27f07ddcefcc71f8d82a1a210620
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-08 19:02:36 +0700

    app: add detail activity

---
+++ app/src/main/java/com/levirs/spacexlaunches/ui/detail/DetailActivity.kt
@@ -0,0 +1,24 @@
+package com.levirs.spacexlaunches.ui.detail
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.levirs.spacexlaunches.R
+import com.levirs.spacexlaunches.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
