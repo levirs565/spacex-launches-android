755a3a5156719588837315c66f7ec7d829fd7bff
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-17 13:38:40 +0700

    app/ui/main: add drawer

--- app/src/main/java/com/levirs/spacexlaunches/ui/main/MainActivity.kt
+++ app/src/main/java/com/levirs/spacexlaunches/ui/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.levirs.spacexlaunches.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.levirs.spacexlaunches.R
 import com.levirs.spacexlaunches.databinding.ActivityMainBinding
 
@@ -11,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.navView.setupWithNavController(navController)
     }
 }
