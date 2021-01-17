afe9b0418499393837e02e8536a7ba1274d32b11
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-17 14:22:35 +0700

    app/ui/main: add hamburger icon

--- app/src/main/java/com/levirs/spacexlaunches/ui/main/MainActivity.kt
+++ app/src/main/java/com/levirs/spacexlaunches/ui/main/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.levirs.spacexlaunches.ui.main
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.levirs.spacexlaunches.R
 import com.levirs.spacexlaunches.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBurgerIcon: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.content.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mBurgerIcon = ActionBarDrawerToggle(this, binding.root, binding.content.toolbar,
+            R.string.drawer_burger_open, R.string.drawer_burger_close)
+        mBurgerIcon.isDrawerIndicatorEnabled = true
+        mBurgerIcon.syncState()
+        binding.root.addDrawerListener(mBurgerIcon)
 
         val navController = findNavController(R.id.nav_host_fragment)
         binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mBurgerIcon.onConfigurationChanged(newConfig)
     }
 }
