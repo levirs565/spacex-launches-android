e6177641c45ae291aefdda8ec1d60e7ed5149d98
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-20 19:51:10 +0700

    app: make coreComponent public

--- app/src/main/java/com/levirs/spacexlaunches/SpaceXApplication.kt
+++ app/src/main/java/com/levirs/spacexlaunches/SpaceXApplication.kt
@@ -3,17 +3,20 @@ package com.levirs.spacexlaunches
 import android.app.Application
 import android.content.Context
 import com.levirs.spacexlaunches.di.AppComponent
+import com.levirs.spacexlaunches.di.CoreComponent
 import com.levirs.spacexlaunches.di.DaggerAppComponent
 import com.levirs.spacexlaunches.di.DaggerCoreComponent
 
 class SpaceXApplication: Application() {
-    private val mCoreComponent by lazy {
+    val coreComponent by lazy {
         DaggerCoreComponent.factory().build(applicationContext)
     }
     val appComponent by lazy {
-        DaggerAppComponent.factory().build(mCoreComponent)
+        DaggerAppComponent.factory().build(coreComponent)
     }
 }
 
 fun Context.getAppComponent(): AppComponent =
     (applicationContext as SpaceXApplication).appComponent
+fun Context.getCoreComponent(): CoreComponent =
+    (applicationContext as SpaceXApplication).coreComponent
