865f31da2925ce281a2d663a7a23119939104265
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-01-23 20:58:15 +0700

    app: sync navigation with toolbar - use AppBarConfiguration, setupWithNavController

--- app/src/main/java/com/levirs/spacexlaunches/ui/main/MainActivity.kt
+++ app/src/main/java/com/levirs/spacexlaunches/ui/main/MainActivity.kt
@@ -1,38 +1,31 @@
 package com.levirs.spacexlaunches.ui.main
 
-import android.content.res.Configuration
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.levirs.spacexlaunches.R
 import com.levirs.spacexlaunches.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mBurgerIcon: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.content.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        mBurgerIcon = ActionBarDrawerToggle(
-            this, binding.root, binding.content.toolbar,
-            R.string.drawer_burger_open, R.string.drawer_burger_close
-        )
-        mBurgerIcon.isDrawerIndicatorEnabled = true
-        mBurgerIcon.syncState()
-        binding.root.addDrawerListener(mBurgerIcon)
 
         val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragmentLaunches, R.id.favoriteFragment
+            ), binding.root
+        )
         binding.navView.setupWithNavController(navController)
+        binding.content.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        title = navController.currentDestination?.label
     }
 
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        mBurgerIcon.onConfigurationChanged(newConfig)
-    }
 }
