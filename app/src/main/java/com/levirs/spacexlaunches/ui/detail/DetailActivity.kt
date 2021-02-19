767bda60856004fad3c66c4145cb3aa09902c681
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-02-19 14:08:09 +0700

    app: make toast when image failed to load in DetailActivity

--- app/src/main/java/com/levirs/spacexlaunches/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/levirs/spacexlaunches/ui/detail/DetailActivity.kt
@@ -1,12 +1,18 @@
 package com.levirs.spacexlaunches.ui.detail
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.levirs.spacexlaunches.R
 import com.levirs.spacexlaunches.core.domain.entity.LaunchEntity
 import com.levirs.spacexlaunches.databinding.ActivityDetailBinding
@@ -18,12 +24,35 @@ import javax.inject.Inject
 
 class DetailActivity : AppCompatActivity() {
     private val mNavArgs: DetailActivityArgs by navArgs()
+
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val mViewModel: DetailViewModel by viewModels {
         viewModelFactory
     }
     private lateinit var mBinding: ActivityDetailBinding
+    private val mGlideListener: RequestListener<Drawable> = object : RequestListener<Drawable> {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            Toast.makeText(this@DetailActivity, "Gambar gagal dimuat", Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            return false
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         getAppComponent().injectDetailActivity(this)
@@ -72,6 +101,7 @@ class DetailActivity : AppCompatActivity() {
 
         Glide.with(this)
             .load(launch.largePatch)
+            .listener(mGlideListener)
             .into(mBinding.imgPatch)
         mBinding.fab.setImageResource(
             if (launch.isFavorite)
